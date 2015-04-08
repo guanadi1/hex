1e1d7c5f71017a78265d488c4a68204455cd6ddb
guanadi1 <guanadi1@gmail.com>
2015-04-08 14:07:41 +0200

    new
    new

--- src/hex/hex.java
+++ src/hex/hex.java
@@ -58,7 +58,7 @@ public class hex {
         InputStream fis = new FileInputStream(inFile);
         OutputStream fos = new FileOutputStream(outFile);
      
-        
+        decryptFile(inFile);
         
 		// decrypt(fis, fos);
 		
@@ -106,116 +106,32 @@ public class hex {
 	   }	
 	  
 	  
-	  public static void decryptFile(String keyString, String fileName){
-		    try {
-		    	
-		    	Cipher c = Cipher.getInstance("AES/CBC/PKCS5Padding", "BC");
-		    	
-		        KeyGenerator kgen = KeyGenerator.getInstance("AES");
-		        kgen.init(128);
-		        SecretKey key = (SecretKey) new SecretKeySpec(
-		            keyString.getBytes(), "AES");// kgen.generateKey();
+	  public static void decryptFile(File file) throws 
+	  NoSuchAlgorithmException, 
+	  NoSuchPaddingException, 
+	  InvalidKeyException, 
+	  IllegalBlockSizeException, 
+	  BadPaddingException, 
+	  IOException{
+		  
+		  byte[] key = aes.getBytes();	  
+		  FileInputStream fis = new FileInputStream(file);
+	      byte[] bFile = new byte[(int) file.length()];
 
-		        AESEncrypter encrypter = new AESEncrypter(key);
+		  SecretKeySpec secretKeySpec = new SecretKeySpec(key, "AES");
 
-		        encrypter.decrypt(new FileInputStream(
-		            new java.io.File("").getCanonicalFile() +
-		            File.separator + "Received"+
-		            File.separator + fileName),
-		            new FileOutputStream(new java.io.File("").getCanonicalFile() +
-		            File.separator + "Decrypted" + 
-		            File.separator + fileName));
-		    } catch (Exception e) {
-		        e.printStackTrace();
-		    }
+		  // Instantiate the cipher
+		  Cipher cipher = Cipher.getInstance("AES/ECB/NoPadding");
+		  cipher.init(Cipher.ENCRYPT_MODE, secretKeySpec);
+
+		  byte[] encrypted = cipher.doFinal(bFile);
+
+		  cipher.init(Cipher.DECRYPT_MODE, secretKeySpec);
+		  byte[] original = cipher.doFinal(encrypted);
+		  
+		  fis.close();
 		}
 
-	  public  void AESEncrypter(SecretKey key) {
-		    // Create an 8-byte initialization vector
-		    byte[] iv = new byte[] { 0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06,
-		            0x07, 0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f };
-
-		    AlgorithmParameterSpec paramSpec = new IvParameterSpec(iv);
-		    try {
-		        ecipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
-		        dcipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
-
-		        // CBC requires an initialization vector
-		        ecipher.init(Cipher.ENCRYPT_MODE, key, paramSpec);
-		        dcipher.init(Cipher.DECRYPT_MODE, key, paramSpec);
-		    } catch (Exception e) {
-		        e.printStackTrace();
-		    }
-		}
 	  
 	  
-	  
-		/*  public static void decrypt(InputStream fis, OutputStream fos)
-      throws IOException,
-      ShortBufferException,
-      IllegalBlockSizeException,
-      BadPaddingException, 
-      InvalidKeyException,
-      NoSuchAlgorithmException, 
-      NoSuchProviderException, 
-      NoSuchPaddingException, 
-      InvalidAlgorithmParameterException
-{
- //get the IV from the file
- // DO NOT FORGET TO reinit the cipher with the IV
- IV= iv.getBytes();
- key= aes.getBytes();
- fis.read(IV,0,IV.length);
- InitCiphers();
-
- byte[] buffer = new byte[blockSize];
- int noBytes = 0;
- byte[] cipherBlock =
-         new byte[decryptCipher.getOutputSize(buffer.length)];
- int cipherBytes;
- while((noBytes = fis.read(buffer))!=-1)
- {
-     cipherBytes =
-             decryptCipher.update(buffer, 0, noBytes, cipherBlock);
-     fos.write(cipherBlock, 0, cipherBytes);
- }
- //allways call doFinal
- cipherBytes = decryptCipher.doFinal(cipherBlock,0);
- fos.write(cipherBlock,0,cipherBytes);
-
- //close the files
- fos.close();
- fis.close();
-}*/
-
-
-	  /*
-	    public static void InitCiphers()
-	            throws NoSuchAlgorithmException,
-	            NoSuchProviderException,
-	            NoSuchProviderException,
-	            NoSuchPaddingException,
-	            InvalidKeyException,
-	            InvalidAlgorithmParameterException{
-	       //1. create the cipher using Bouncy Castle Provider
-	       encryptCipher =
-	               Cipher.getInstance("AES/CBC/PKCS5Padding");
-	       //2. create the key
-	       
-	       byte[] encodedKey  = aes.getBytes();
-	       SecretKey originalKey = new SecretKeySpec(encodedKey, 0, encodedKey.length, "AES");
-	       SecretKey keyValue = new SecretKeySpec(key,"AES");
-	       //3. create the IV
-	       AlgorithmParameterSpec IVspec = new IvParameterSpec(IV);
-	       //4. init the cipher
-	       encryptCipher.init(Cipher.ENCRYPT_MODE, originalKey, IVspec);
-
-	       //1 create the cipher
-	       decryptCipher =Cipher.getInstance("AES/CBC/PKCS5Padding");
-	       //2. the key is already created
-	       //3. the IV is already created
-	       //4. init the cipher
-	       decryptCipher.init(Cipher.DECRYPT_MODE, originalKey, IVspec);
-	    }*/
-
 }
